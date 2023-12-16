2ff244aa5c1e4b839bcab3e7e0a6007f9f28dfb9
Davyd Zakorchennyi <davyd.zakorchennyi@chartboost.com>
2023-12-16 13:39:59 -0500

    landing -> moved mouse movement functions into util

--- src/pages/Landing/ProjectsGrid/ProjectBox/index.tsx
+++ src/pages/Landing/ProjectsGrid/ProjectBox/index.tsx
@@ -2,38 +2,16 @@ import React from 'react';
 import S from './styles';
 import { Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { onProjectsTileMouseMove, onProjectsTileMouseOut, onProjectsTileMouseOver } from './utils';
 
 interface ProjectInfo {
   category: string;
   imagePath: string;
 }
 
-type ProjectBoxMouseEvent = React.MouseEvent<HTMLImageElement, MouseEvent>;
-
 const ProjectBox = (projectInfo: ProjectInfo) => {
   const { category, imagePath } = projectInfo;
-  const onMouseOver = (e: ProjectBoxMouseEvent) => {
-    if (e.target === null) return;
 
-    const projectImage = e.target as HTMLImageElement;
-    projectImage.style.transform = 'scale(1.15)';
-  };
-  const onMouseOut = (e: ProjectBoxMouseEvent) => {
-    if (e.target === null) return;
-
-    const projectImage = e.target as HTMLImageElement;
-    projectImage.style.transform = 'scale(1)';
-  };
-  const onMouseMove = (e: ProjectBoxMouseEvent) => {
-    if (e.target === null) return;
-    
-    const projectImage = e.target as HTMLImageElement;
-    const projectImageBoundingBox = projectImage.getBoundingClientRect();
-    const transformOriginX = (e.clientX - projectImageBoundingBox.left) / projectImageBoundingBox.width * 100;
-    const transformOriginY = (e.clientY - projectImageBoundingBox.top) / projectImageBoundingBox.height * 100;
-
-    projectImage.style.transformOrigin = `${transformOriginX}% ${transformOriginY}%`;
-  };
 
   return (
     <S.ProjectBox className="ProjectBox-Container">
@@ -42,9 +20,9 @@ const ProjectBox = (projectInfo: ProjectInfo) => {
           src={imagePath} 
           className='ProjectBox-Img'
           draggable={false} 
-          onMouseOver={onMouseOver}
-          onMouseOut={onMouseOut}
-          onMouseMove={onMouseMove}
+          onMouseOver={onProjectsTileMouseOver}
+          onMouseOut={onProjectsTileMouseOut}
+          onMouseMove={onProjectsTileMouseMove}
         />
         <Typography className="Category-Name">{`#${category}`}</Typography>
       </Link>
---
+++ src/pages/Landing/ProjectsGrid/ProjectBox/utils.ts
@@ -0,0 +1,24 @@
+type ProjectBoxMouseEvent = React.MouseEvent<HTMLImageElement, MouseEvent>;
+
+export const onProjectsTileMouseOver = (e: ProjectBoxMouseEvent) => {
+  if (e.target === null) return;
+
+  const projectImage = e.target as HTMLImageElement;
+  projectImage.style.transform = 'scale(1.15)';
+};
+export const onProjectsTileMouseOut = (e: ProjectBoxMouseEvent) => {
+  if (e.target === null) return;
+
+  const projectImage = e.target as HTMLImageElement;
+  projectImage.style.transform = 'scale(1)';
+};
+export const onProjectsTileMouseMove = (e: ProjectBoxMouseEvent) => {
+  if (e.target === null) return;
+  
+  const projectImage = e.target as HTMLImageElement;
+  const projectImageBoundingBox = projectImage.getBoundingClientRect();
+  const transformOriginX = (e.clientX - projectImageBoundingBox.left) / projectImageBoundingBox.width * 100;
+  const transformOriginY = (e.clientY - projectImageBoundingBox.top) / projectImageBoundingBox.height * 100;
+
+  projectImage.style.transformOrigin = `${transformOriginX}% ${transformOriginY}%`;
+};
