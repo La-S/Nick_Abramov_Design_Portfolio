68df37a56f9eb4c6eb5b1e47d30aec49974ce301
Davyd Zakorchennyi <davyd.zakorchennyi@chartboost.com>
2024-01-06 15:28:18 -0500

    added production server link

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 import Cookies from 'universal-cookie';
 export const cookies = new Cookies();
 
-const baseURL = 'http://localhost:8080';
+const PRODUCTION_SERVER_URL = 'https://nick-abramov-design-portfolio-server.vercel.app';
+const DEVELOPMENT_SERVER_URL = 'http://localhost:8080';
+const baseURL = process.env.NODE_ENV == 'production' ? PRODUCTION_SERVER_URL : DEVELOPMENT_SERVER_URL;
 
 const api = axios.create({
   baseURL,
