577582e63b2875a381ec1f23beb03efd024b05e1
Davyd Zakorchennyi <davyd.zakorchennyi@chartboost.com>
2024-01-20 11:48:51 -0500

    Updated FAQs

--- src/pages/Faq/QuestionsLists/index.tsx
+++ src/pages/Faq/QuestionsLists/index.tsx
@@ -9,36 +9,41 @@ const QUESTIONS_AND_ANSWERS: Array<{ question: string; answer: string }> = [
   {
     question: 'What services do you offer?',
     answer:
-      'I offer the following services: Consultation, Logo Design,' +
-      'Logo Animation, Brand Identity, Website Design, Illustrations, Portraits, and more.',
+      `I offer the following services: Consultation, Logo Design, Logo Animation, 
+      Brand Identity, Website Design, Illustrations, Portraits, and more.`,
   },
   {
     question: 'How many years of experience do you have?',
-    answer: 'I have 10+ years of experience in web design and development.',
+    answer: `I started learning graphic design back in 2015. After a few years learning, 
+    I started taking freelance projects and improving my skills little by little. 
+    Now, I’m confident in my skills and can provide good service for you.`,
   },
   {
     question: 'Do you have a design degree?',
-    answer: 'I have 10+ years of experience in web design and development.',
+    answer: 'Currently, I’m working on a bachelors degree in Visual Communications and Experience Design at Montreat College',
   },
   {
     question: 'How long does it take?',
-    answer: 'I have 10+ years of experience in web design and development.',
+    answer: 'Depending on what project you have, it will take from 1 week to 6 weeks for a big project',
   },
   {
     question: 'Do you work with international clients?',
-    answer: 'I have 10+ years of experience in web design and development.',
+    answer: `Yes, I do. I had clients from all over the World. For international payment, the easiest way for me 
+    is PayPal, but we can discuss different options too.`,
   },
   {
     question: 'Can you give me feedback on my current logo?',
-    answer: 'I have 10+ years of experience in web design and development.',
-  },
-  {
-    question: 'Can you fix up my current logo?',
-    answer: 'I have 10+ years of experience in web design and development.',
+    answer: `Absolutely, I offer consultation meetings where you can get detailed feedback on your logo 
+    and what could be done to improve it, as well as ask any questions you may have.`,
   },
   {
     question: 'I can’t find what I’m looking for, what should I do?',
-    answer: 'I have 10+ years of experience in web design and development.',
+    answer: `I hope I've covered all the main questions I usually get asked, but if I've missed anything, 
+    don't hesitate to get in touch with me via the contact form or email me; I'll be happy to help!`,
+  },
+  {
+    question: 'Can you fix up my current logo?',
+    answer: 'Yes, let’s get in touch and discuss what we can do.',
   },
 ];
 
