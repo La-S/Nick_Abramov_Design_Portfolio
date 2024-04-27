add348253c482455b10bb19ffc9d1f415133fb0a
Davyd Zakorchennyi <Diza41a@mail.com>
2024-04-27 15:30:31 -0400

    MVP contact form result toast

--- src/pages/Contact/ContactForm/index.tsx
+++ src/pages/Contact/ContactForm/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Snackbar, Alert } from '@mui/material';
+import type { SnackbarProps } from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
 import { TextField, Typography, Button } from '@mui/material';
 import S from './styles';
@@ -13,6 +15,16 @@ import type { EmailInputDto } from '../../../types/data/emailAPI';
 import { sendEmail } from '../../../api/emailMethods.api';
 
 const ContactForm = (): JSX.Element => {
+  const [snackbarProps, setSnackbarProps] = useState<SnackbarProps & { error: boolean }>({
+    open: false,
+    message: '',
+    error: false,
+    autoHideDuration: 3500,
+    anchorOrigin: {
+      vertical: 'bottom',
+      horizontal: 'right',
+    },
+  });
   const {
     control,
     formState: { errors },
@@ -28,101 +40,131 @@ const ContactForm = (): JSX.Element => {
     mode: 'onChange',
   });
   const onContactFormSubmit = async (emailInputDto: EmailInputDto) => {
-    sendEmail(emailInputDto);
+    sendEmail(emailInputDto)
+      .then(() => setSnackbarProps({
+        ...snackbarProps,
+        open: true,
+        message: 'Your inquiry has been sent!',
+        error: false,
+      }))
+      .catch(() => {
+        setSnackbarProps({
+          ...snackbarProps,
+          open: true,
+          message: 'Internal error. Please try again later, or reach out directly.',
+          error: true,
+        });
+      });
   };
 
   return (
-    <S.ContactForm onSubmit={handleSubmit(onContactFormSubmit)}>
-      <Typography className="Contact-Form-Label">
-        If you would like to find out more about how I can help your company, please get in touch below, and I’d be
-        happy to help:
-      </Typography>
+    <>
+      {snackbarProps.open ? (
+        <Snackbar
+          {...snackbarProps}
+          onClose={() => setSnackbarProps({ ...snackbarProps, open: false })}
+        >
+          <Alert
+            severity={snackbarProps.error ? 'error' : 'success'}
+            variant="filled"
+          >
+            {snackbarProps.message}
+          </Alert>
+        </Snackbar>
+      ) : <></>}
 
-      <Controller
-        name="name"
-        control={control}
-        rules={{
-          ...generateRequiredRule('Name'),
-          ...generateMinLengthRule('Name', 3),
-          ...generateMaxLengthRule('Name', 50),
-        }}
-        render={({ field }) => (
-          <TextField {...field} variant="outlined" placeholder="Name*" {...generateTextfieldErrorProps(errors.name)} />
-        )}
-      />
-      <Controller
-        name="companyName"
-        control={control}
-        rules={{
-          ...generateMinLengthRule('Company Name', 3),
-          ...generateMaxLengthRule('Company Name', 50),
-        }}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            variant="outlined"
-            placeholder="Company Name"
-            {...generateTextfieldErrorProps(errors.companyName)}
-          />
-        )}
-      />
-      <Controller
-        name="contactEmail"
-        control={control}
-        rules={{
-          ...generateRequiredRule('Email'),
-          ...generateEmailRule(),
-        }}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            variant="outlined"
-            placeholder="Contact Email*"
-            {...generateTextfieldErrorProps(errors.contactEmail)}
-          />
-        )}
-      />
-      <Controller
-        name="servicesRequired"
-        control={control}
-        rules={{
-          ...generateRequiredRule('Services Required'),
-          ...generateMinLengthRule('Services Required', 4),
-          ...generateMaxLengthRule('Services Required', 100),
-        }}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            variant="outlined"
-            placeholder="Services Required*"
-            {...generateTextfieldErrorProps(errors.servicesRequired)}
-          />
-        )}
-      />
-      <Controller
-        name="projectDetails"
-        control={control}
-        rules={{
-          ...generateRequiredRule('Project Details'),
-          ...generateMinLengthRule('Project Details', 10),
-          ...generateMaxLengthRule('Project Details', 500),
-        }}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            variant="outlined"
-            placeholder="Project Details*"
-            multiline
-            rows={4}
-            {...generateTextfieldErrorProps(errors.projectDetails)}
-          />
-        )}
-      />
+      <S.ContactForm onSubmit={handleSubmit(onContactFormSubmit)}>
+        <Typography className="Contact-Form-Label">
+          If you would like to find out more about how I can help your company, please get in touch below, and I’d be
+          happy to help:
+        </Typography>
 
-      <Button className="Submit-Button" type="submit">
-        Send
-      </Button>
-    </S.ContactForm>
+        <Controller
+          name="name"
+          control={control}
+          rules={{
+            ...generateRequiredRule('Name'),
+            ...generateMinLengthRule('Name', 3),
+            ...generateMaxLengthRule('Name', 50),
+          }}
+          render={({ field }) => (
+            <TextField {...field} variant="outlined" placeholder="Name*" {...generateTextfieldErrorProps(errors.name)} />
+          )}
+        />
+        <Controller
+          name="companyName"
+          control={control}
+          rules={{
+            ...generateMinLengthRule('Company Name', 3),
+            ...generateMaxLengthRule('Company Name', 50),
+          }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              placeholder="Company Name"
+              {...generateTextfieldErrorProps(errors.companyName)}
+            />
+          )}
+        />
+        <Controller
+          name="contactEmail"
+          control={control}
+          rules={{
+            ...generateRequiredRule('Email'),
+            ...generateEmailRule(),
+          }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              placeholder="Contact Email*"
+              {...generateTextfieldErrorProps(errors.contactEmail)}
+            />
+          )}
+        />
+        <Controller
+          name="servicesRequired"
+          control={control}
+          rules={{
+            ...generateRequiredRule('Services Required'),
+            ...generateMinLengthRule('Services Required', 4),
+            ...generateMaxLengthRule('Services Required', 100),
+          }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              placeholder="Services Required*"
+              {...generateTextfieldErrorProps(errors.servicesRequired)}
+            />
+          )}
+        />
+        <Controller
+          name="projectDetails"
+          control={control}
+          rules={{
+            ...generateRequiredRule('Project Details'),
+            ...generateMinLengthRule('Project Details', 10),
+            ...generateMaxLengthRule('Project Details', 500),
+          }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              placeholder="Project Details*"
+              multiline
+              rows={4}
+              {...generateTextfieldErrorProps(errors.projectDetails)}
+            />
+          )}
+        />
+
+        <Button className="Submit-Button" type="submit">
+          Send
+        </Button>
+      </S.ContactForm>
+    </>
   );
 };
 
