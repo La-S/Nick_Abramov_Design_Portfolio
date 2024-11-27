8e6b3c6bcda76fc2efa37ec6316c7875260a5abf
Davyd Zakorchennyi <Diza41a@mail.com>
2024-11-27 15:36:01 -0600

    @feat: Photo Blog - Admin photo blog page skeleton;

--- src/pages/AdminPBProjects/index.tsx
+++ src/pages/AdminPBProjects/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ButtonBase } from '@mui/material';
 import S, { classes } from './styles';
+import usePBProjects from '../../hooks/usePBProjects';
 
 const AdminPBProjects = (): JSX.Element => {
+  const [newPBProjectModalOpen, setNewPBProjectModalOpen] = useState(false);
+  const { pBProjects, isFetched } = usePBProjects({ summary: true });
+
+  const NewPBProjectButton = (
+    <div className={classes.newPBProjectButtonWrapper}>
+      <ButtonBase
+        className={classes.newPBProjectButton}
+        // onClick={() => setPBNewProjectModalOpen(true)}
+        disableRipple
+      >
+        <p>
+          new pb project <p>&nbsp;+</p>
+        </p>
+      </ButtonBase>
+    </div>
+  );
 
   return (
-    <S.AdminPBProjects className={classes.root}>
-    </S.AdminPBProjects>
+    <S.AdminPBProjectsContainer className={classes.root}>
+      {NewPBProjectButton}
+
+      {isFetched && pBProjects.length === 0 ? (
+        <div className="No-Projects-Box">
+          <p>{'Click new pb project button to get started...'}</p>
+        </div>
+      ) : (
+        <></>
+      )}
+
+    </S.AdminPBProjectsContainer>
   );
 };
 
--- src/pages/AdminPBProjects/styles.ts
+++ src/pages/AdminPBProjects/styles.ts
@@ -1,14 +1,61 @@
 import { styled } from '@mui/material';
 
 export const classes = {
-  root: 'AdminPBProjects',
-  
+  root: 'AdminPBProjects-Container',
+  newPBProjectButtonWrapper: 'AdminPBProjects-NewPBProjectButtonWrapper',
+  newPBProjectButton: 'AdminPBProjects-NewPBProjectButton',
 };
 
-const AdminPBProjects = styled('div')(() => ({
+const AdminPBProjectsContainer = styled('div')(({ theme }) => ({
+  '.No-Projects-Box': {
+    padding: '15px 35px',
 
+    '.MuiTypography-root': {
+      fontFamily: theme.fonts.main,
+      fontSize: '20px',
+    },
+  },
+
+  [`.${classes.newPBProjectButtonWrapper}`] : {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+
+    [`.${classes.newPBProjectButton}`]: {
+      margin: '15px',
+      padding: '5px 7.5px',
+      backgroundColor: theme.componentColors.backgroundSecondary,
+
+      p: {
+        display: 'flex',
+        margin: 0,
+        fontSize: '16px',
+        fontFamily: theme.fonts.secondary,
+
+        p: {
+          margin: 0,
+        },
+
+        transition: 'color 0.2s ease-in-out',
+        '&:hover': {
+          color: theme.componentColors.backgroundMain,
+
+          transition: 'color 0.2s ease-in-out',
+          p: {
+            color: theme.componentColors.backgroundMain,
+          },
+        },
+      },
+    },
+  },
+
+  [theme.breakpoints.down('tablet')]: {
+    [`.${classes.newPBProjectButtonWrapper}`] : {
+      justifyContent: 'center'
+    }
+  },
 }));
 
 export default {
-  AdminPBProjects,
+  AdminPBProjectsContainer,
 };
