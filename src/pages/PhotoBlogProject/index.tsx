e3a2a9d61506506ec368a550c43ac9fb7e453983
Davyd Zakorchennyi <Diza41a@mail.com>
2024-11-27 12:55:51 -0600

    @feat: Photo Blog -> Loading will trigger or not trigger appropriately on photo blog project navigation;

--- src/pages/PhotoBlogProject/index.tsx
+++ src/pages/PhotoBlogProject/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import usePhotoBlogProject from '../../hooks/usePhotoBlogProject';
-// import { GlobalContext } from '../../contexts/global';
+import { GlobalContext } from '../../contexts/global';
 import { generatePhotoBlogProjectDateString } from '../PhotoBlogProjects/utils';
 import S, { classes } from './styles';
 import PhotoBlogProjectGalleryGrid from './PhotoBlogProjectGalleryGrid';
@@ -10,11 +10,14 @@ import { Video, Zoom } from 'yet-another-react-lightbox/plugins';
 import { getPhotoBlogProjectLightboxSlides } from './utils';
 import { LightboxContext } from './context';
 import PhotoBlogProjectNav from './PhotoBlogProjectNav';
+import { executeCallbackOnMediaCollectionLoad } from '../../utils/loadingUtils';
+
+const MAX_LOADING_DELAY = 2500;
 
 const PhotoBlogProjectPage = (): JSX.Element => {
-  // const {
-  //   pageLoadingState: [isPageLoading, setIsPageLoading],
-  // } = useContext(GlobalContext);
+  const {
+    pageLoadingState: [isPageLoading, setIsPageLoading],
+  } = useContext(GlobalContext);
   const navigate = useNavigate();
   const { state } = useLocation();
   const photoBlogProjectId = state?.photoBlogProjectId;
@@ -22,7 +25,7 @@ const PhotoBlogProjectPage = (): JSX.Element => {
 
   const {
     photoBlogProject,
-    // ...photoBlogProjectResponse
+    ...photoBlogProjectResponse
   } = usePhotoBlogProject(photoBlogProjectId || '');
   const {
     dateInfo,
@@ -31,19 +34,21 @@ const PhotoBlogProjectPage = (): JSX.Element => {
     description,
     gallerySections,
   } = photoBlogProject || {};
-  const formattedDateString = generatePhotoBlogProjectDateString(dateInfo);
+  const [isLoadingDelayActive, setIsLoadingDelayActive] = useState(true);
+  const [areImagesLoaded, setAreImagesLoaded] = useState(false);
 
   const [isLightboxOpen, setIsLightboxOpen] = useState(false);
   const [slideIndex, setSlideIndex] = useState(0);
   const slides = getPhotoBlogProjectLightboxSlides(photoBlogProject);
 
+  const formattedDateString = generatePhotoBlogProjectDateString(dateInfo);
   const OverviewSection = (
     <div className={classes.overviewContainer}>
       <h5 className={classes.dateCreated}>{formattedDateString}</h5>
       <h2 className={classes.name}>{nameInfo.full}</h2>
       <p className={classes.description}>{description}</p>
       <img
-        className={classes.mainImage}
+        className={`${classes.mainImage} Loadable-Image`}
         src={mainImage.path}
         onClick={() => {
           setIsLightboxOpen(true);
@@ -53,6 +58,34 @@ const PhotoBlogProjectPage = (): JSX.Element => {
       />
     </div>
   );
+
+  useEffect(() => {
+    if (!isPageLoading
+      || photoBlogProjectResponse.isLoading
+      || isLoadingDelayActive
+      || !areImagesLoaded) return;
+
+    setIsPageLoading(false);
+  }, [photoBlogProjectResponse.isLoading, isLoadingDelayActive, areImagesLoaded]);
+
+  useEffect(() => {
+    setIsLoadingDelayActive(true);
+
+    setTimeout(() => {
+      setIsLoadingDelayActive(false);
+    }, MAX_LOADING_DELAY);
+  }, [photoBlogProjectId]);
+
+  useEffect(() => {
+    if (photoBlogProjectResponse.isLoading) return;
+
+    const mediaToLoad: Array<HTMLImageElement | HTMLVideoElement> = Array.from(
+      document.querySelectorAll('.Loadable-Image'),
+    );
+    executeCallbackOnMediaCollectionLoad(mediaToLoad, () => {
+      setAreImagesLoaded(true);
+    });
+  }, [photoBlogProject]);
 
   return (
     <LightboxContext.Provider
